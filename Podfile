6de1a06bea8a9835f3a6f83c7a6c85ca3d888832
remi <remirobert33530@gmail.com>
2017-04-01 12:13:02 +0100

    add base project. AWS polly depedency

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'ios-client-rap' do
+  use_frameworks!
+  pod 'AWSPolly', '~> 2.5.0'
+end
---
+++ ios-client-rap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-client-rap
+//
+//  Created by Remi Robert on 01/04/2017.
+//  Copyright © 2017 Remi Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
